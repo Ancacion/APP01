7efc5d01b4dfa2cfcf298f20c3aa717b4284fff2
Ancacion <joker103186@gmail.com>
2018-04-25 17:47:27 +0800

    app01

--- app/src/test/java/com/example/user/hw1/SuggestionTest.java
+++ app/src/test/java/com/example/user/hw1/SuggestionTest.java
@@ -1,16 +1,28 @@
 package com.example.user.hw1;
 
+import org.junit.After;
+import org.junit.Before;
+
 /**
  * Created by NTUTCSIE on 2018/4/25.
  */
 
 public class SuggestionTest {
+    private Suggestion ms;
 
-    @Test
+    @Before
+    public void setUp(){
+        ms = new Suggestion();
+    }
+
+    @After
+    public void tearDown(){
+        ms = null;
+    }
     public void getSuggestion(){
         Suggestion gradeSuggestion = new Suggestion();
-        String s = gradeSuggestion.getSuggestion(-10);
-        assertEquals(s, "輸入錯誤");
+        String s = gradeSuggestion.getSuggestion(10);
+        //aassertEquals(s, ms.getSuggestion(10));
 
     }
 }
