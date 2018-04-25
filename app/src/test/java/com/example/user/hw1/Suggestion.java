8c59c5ff3a69d2c9dc0cd124e314cb61c16e6987
Ancacion <joker103186@gmail.com>
2018-04-25 17:28:06 +0800

    app01

--- app/src/test/java/com/example/user/hw1/Suggestion.java
+++ app/src/test/java/com/example/user/hw1/Suggestion.java
@@ -5,6 +5,24 @@ package com.example.user.hw1;
  */
 
 public class Suggestion {
+    public String getSuggestion(int grade)
+    {
+        String strSug = "";
+        if(grade < 0)
+            strSug += "輸入錯誤";
+        else if(grade >= 0 && grade <= 59)
+            strSug += " = F";
+        else if(grade >= 60 && grade <= 69)
+            strSug += " = D";
+        else if(grade >= 70 && grade <= 79)
+            strSug += " = C";
+        else if(grade >= 80 && grade <= 89)
+            strSug += " = B";
+        else if(grade >= 90 && grade <= 100)
+            strSug += " = A";
+        else if(grade > 100)
+            strSug += "輸入錯誤";
 
-
+        return strSug;
+    }
 }
